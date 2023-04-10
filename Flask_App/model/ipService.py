8ff4ec516b068c4960f5c5c6840e034c0a876a3e
ZhengJie98 <88995146+ZhengJie98@users.noreply.github.com>
2023-04-10 21:26:20 +0800

    finished ylsit

--- Flask_App/model/ipService.py
+++ Flask_App/model/ipService.py
@@ -22,7 +22,8 @@ import requests
 from bs4 import BeautifulSoup
 from pathlib import Path
 import time
-
+from pathvalidate import sanitize_filepath
+import chardet
 
 
 
@@ -52,8 +53,8 @@ ip_template = {
         
     }
 
-# API_KEY = '0d9fdb6e32d74b9d12e3d894309531838c3aabe8d66b049fd3a7976fbedf2c68'  #@param  {type: "string"}
-API_KEY = '207349263f9c5edd176cc079fa8000a5ab912df7d9e91154842c08031658675d'  #@param  {type: "string"}
+API_KEY = '0d9fdb6e32d74b9d12e3d894309531838c3aabe8d66b049fd3a7976fbedf2c68'  #@param  {type: "string"}
+# API_KEY = '207349263f9c5edd176cc079fa8000a5ab912df7d9e91154842c08031658675d'  #@param  {type: "string"}
 
 
 
@@ -163,7 +164,7 @@ def process_ip_parent():
                     time.sleep(15-(toc-tic))
                     toc = time.perf_counter()
 
-                print(f"replacement SUCCESSFUL for {ip}, time taken {toc-tic} seconds")
+                print(f"replacement SUCCESSFUL for {ip}, time taken {toc-tic} seconds\n\n\n")
             # cursor.close()
 
             # if call_ip_status_code == 429:
@@ -186,7 +187,7 @@ def retrieve_ips_to_process(how_many_ips):
     # ).sort("added_timestamp", pymongo.ASCENDING).limit(how_many_ips)
     
     # cursor = col.find(
-    #     {"$and" : [{"ip_address" : "193.163.125.111"},{"processed_timestamp" : ""}, {"to_skip" : ""}, {"is_priority" : 0}]}
+    #     {"$and" : [{"ip_address" : "80.252.0.252"},{"processed_timestamp" : ""}, {"to_skip" : ""}, {"is_priority" : 0}]}
     #     ).sort("added_timestamp", pymongo.ASCENDING).limit(how_many_ips)
     
     # print("===== retrieve_ips_to_process() END =====")
@@ -201,12 +202,12 @@ def call_ip(ip_doc, x_days_ago):
     ip = str(ip_doc["ip_address"])
     db_id = ip_doc['_id']
 
-    print("calling ip for:", ip )
-
     now = datetime.datetime.now()
     dt_string = now.strftime("%d%m%Y")
     d = datetime.timedelta(days = x_days_ago)
     deducted_date = (now - d).strftime("%d%m%Y")
+
+    print("calling ip for:", ip , "now:", now)
 
     if not os.path.exists("resources/downloaded_vtresponse"):
         os.makedirs("resources/downloaded_vtresponse")
@@ -471,6 +472,7 @@ def grab_html_js(ip_doc):
 
 
     for protocol in ["http", "https"]:
+        print("current protocol:", protocol)
 
         if protocol == "http":
             filepath = "resources/html/*ip*_http_*dt_string*.html"
@@ -491,6 +493,7 @@ def grab_html_js(ip_doc):
             e
 
         returncode = response.returncode
+        print("return code:", returncode)
 
         if returncode == 0:
             ip_doc['has_html'] = 1
@@ -508,50 +511,80 @@ def grab_html_js(ip_doc):
 
             # html = requests.get(web_url).content
             try:
+                print("starting html request.get")
                 html = requests.get(web_url).content
             
 
                 # parse HTML Content
                 soup = BeautifulSoup(html, "html.parser")
                 
-                js_files_link = []
-                js_filename_alone = []
+                js_files_link = []  #['https://biv.gazeta.pl/static/front/ot-iab-consent/master/rodoGuard-ux.js?v17', 'https://cdn.cookielaw.org/scripttemplates/otSDKStub.js', 'https://biv.gazeta.pl/static/front/ot-iab-consent/master/ot-ux-fix.js?v27']
+                js_filename_alone = [] # ['https//biv.gazeta.pl/static/front/ot-iab-consent/master/rodoGuard-ux_20230410_154604.047', 'https//cdn.cookielaw.org/scripttemplates/otSDKStub_20230410_154604.047']
                 for script in soup.find_all("script"):
                     if script.attrs.get("src"):
                         
                         # if the tag has the attribute
                         # 'src'
                         url = script.attrs.get("src")
-                        js_files_link.append(web_url+url)
-                        js_filename_alone.append(url.split('.js')[0] + '_' + dt_string) 
+                        # js_files_link.append(web_url+url)
+                        if url[0:4] == "http":
+                            js_files_link.append(url)
+                        else:    
+                            js_files_link.append(web_url+url)
+                    
+                        # js_filename_alone.append(url.split('.js')[0].replace(":","") + '_' + dt_string) 
+                        js_filename_alone.append(sanitize_filepath(url.split('.js')[0], platform="auto")+ '_' + dt_string) 
+
                 # print(js_files)
                 js_file_counter = 0
                 array_js_filenames = []
+                js_files_link = list(dict.fromkeys(js_files_link))
+                js_filename_alone = list(dict.fromkeys(js_filename_alone))
                 print("total js_files_links:", js_files_link)
                 for each_js in js_files_link:
-                    print("current each_js:", each_js)
-                    each_js = requests.get(each_js).content
-                    # print(type(js.decode()))
-                    js_file_path = "resources/js/" + protocol + js_filename_alone[js_file_counter] + ".js"
-                    array_js_filenames.append("resources/js/" + protocol + js_filename_alone[js_file_counter] + ".js")
+                    print("starting get for each_js:", each_js)
+                    try:
 
-                    ## testing new folder creation
-                    # if not os.path.exists('resources/js'):
-                    #     os.mkdir('resources/js')
-                    # if not os.path.exists('resources/js/' + protocol):
-                    #     os.mkdir('resources/js/' + protocol)
-                    output_file = Path(js_file_path)
-                    output_file.parent.mkdir(exist_ok=True, parents=True)
+                        each_js = requests.get(each_js).content
+                        # print(type(js.decode()))
+                        # print("each_js gotten")
+                        # js_file_path = "resources/js/" + protocol + js_filename_alone[js_file_counter] + ".js"
+                        # array_js_filenames.append("resources/js/" + protocol + js_filename_alone[js_file_counter] + ".js")
 
-                    with open(js_file_path, "w", encoding="utf-8") as f:
-                    # with open(js_file_path, "w") as f:
-                        
-                        # text_file = open(js_file_path, "w+")
-                        f.write(each_js.decode())
-                        # f.close()
+                        if url[0:4] == "http":
+                            js_file_path = "resources/js/" + protocol + '/' + js_filename_alone[js_file_counter] + ".js"
+                            array_js_filenames.append("resources/js/" + protocol +'/'+ js_filename_alone[js_file_counter] + ".js")
+                        else:
+                            js_file_path = "resources/js/" + protocol + js_filename_alone[js_file_counter] + ".js"
+                            array_js_filenames.append("resources/js/" + protocol + js_filename_alone[js_file_counter] + ".js")
+                                        
+                        ## testing new folder creation
+                        # if not os.path.exists('resources/js'):
+                        #     os.mkdir('resources/js')
+                        # if not os.path.exists('resources/js/' + protocol):
+                        #     os.mkdir('resources/js/' + protocol)
+                        output_file = Path(js_file_path)
+                        output_file.parent.mkdir(exist_ok=True, parents=True)
 
-                    js_file_counter += 1
+                        encoding = chardet.detect(each_js)['encoding']
+                    # encoding = json.detect_encoding(each_js)
 
+                    # print("encoding:", encoding)
+                        # the_encoding = chardet.detect(rawdata)['encoding']
+
+                    # print(type(each_js))
+
+                        with open(js_file_path, "w", encoding=encoding, errors='ignore') as f:
+                        # with open(js_file_path, "w") as f:
+                            # with open(js_file_path, "w") as f:
+                                
+                            # text_file = open(js_file_path, "w+")
+                            f.write(each_js.decode(errors='ignore'))
+                            # f.close()
+
+                        js_file_counter += 1
+                    except Exception as e:
+                        print("exception in getting js_file:", e)
 
                 if len(array_js_filenames) > 0:
                     # ip_doc['has_javascript'] = len(array_js_filenames)
--- Flask_App/test3.py
+++ Flask_App/test3.py
@@ -21,13 +21,177 @@ import requests
 from bs4 import BeautifulSoup
 from datetime import timedelta
 import time
+from pathlib import Path
+from pathvalidate import sanitize_filepath
+import chardet
 
-tic = time.perf_counter()
 
-toc = time.perf_counter()
+# print("===== grab_html_js function start =====")
+now = datetime.datetime.now()
+dt_string = now.strftime("%Y%m%d_%H%M%S.%f")[:-3]   
+# ip_address = str(ip_doc["ip_address"])
+# db_id = ip_doc['_id']  
+# ip_address = "1.1.1.1"
+ip_address = "80.252.0.252"
 
-if (toc-tic) < 15:
-    balance = 15-(toc-tic)
-    print("sleeping to makeup 15 seconds: ", balance, "seconds" )
-    time.sleep(balance)
-# print("Finished " + ip + "in " f"{toc - tic:0.4f} seconds")
+
+for protocol in ["http", "https"]:
+    print("current protocol:", protocol)
+
+    if protocol == "http":
+        filepath = "resources/html/*ip*_http_*dt_string*.html"
+        filepath = filepath.replace('*ip*', ip_address)
+        filepath = filepath.replace('*dt_string*', dt_string)
+        query = "curl -i http://{ip} --create-dirs -o {filepath}".format(ip=ip_address, filepath = filepath)
+
+    elif protocol == "https":
+        filepath = "resources/html/*ip*_https_*dt_string*.html"
+        filepath = filepath.replace('*ip*', ip_address)
+        filepath = filepath.replace('*dt_string*', dt_string)
+        query = "curl -i https://{ip} --create-dirs -o {filepath}".format(ip=ip_address, filepath = filepath)
+
+    try:
+        response = subprocess.run(query, shell=False, capture_output=True, text=True)
+    except Exception as e:
+        print("grab_html_js() exception triggered:", e)
+        e
+
+    returncode = response.returncode
+    print("return code:", returncode)
+
+    if returncode == 0:
+        # ip_doc['has_html'] = 1
+        to_append = {"type": protocol, "stderr": response.stderr, "stdout": response.stdout, "html_file_location":filepath}
+
+
+        ## Grab JS HERE
+        web_url = protocol + "://" + ip_address
+        # headers = {
+        #             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:66.0) Gecko/20100101 Firefox/66.0",
+        #             "Accept-Encoding": "*",
+        #             "Connection": "keep-alive"
+        #         }
+        # html = requests.get(web_url, headers=headers).content
+
+        # html = requests.get(web_url).content
+        try:
+            print("starting html request.get")
+            html = requests.get(web_url).content
+        
+
+            # parse HTML Content
+            soup = BeautifulSoup(html, "html.parser")
+            
+            js_files_link = []
+            js_filename_alone = []
+            for script in soup.find_all("script"):
+                if script.attrs.get("src"):
+                    
+                    # if the tag has the attribute
+                    # 'src'
+                    url = script.attrs.get("src")
+                    print("web_url:", web_url)
+                    print("url:", url)
+                    
+                    if url[0:4] == "http":
+                        js_files_link.append(url)
+                        
+                    else:    
+                        js_files_link.append(web_url+url)
+                   
+                    # js_filename_alone.append(url.split('.js')[0]+ '_' + dt_string) 
+                    js_filename_alone.append(sanitize_filepath(url.split('.js')[0], platform="auto")+ '_' + dt_string) 
+                    
+                    
+            # print(js_files)
+            js_file_counter = 0
+            js_files_link = list(dict.fromkeys(js_files_link))
+            js_filename_alone = list(dict.fromkeys(js_filename_alone))
+
+            array_js_filenames = []
+            # print("total js_files_links:", js_files_link)
+            # print("js_filename_alone", js_filename_alone)
+            for each_js in js_files_link:
+                print("starting get for each_js:", each_js)
+                try:
+
+                    each_js = requests.get(each_js).content
+                    # print(type(js.decode()))
+                    print("each_js gotten")
+                    if url[0:4] == "http":
+                        js_file_path = "resources/js/" + protocol + '/' + js_filename_alone[js_file_counter] + ".js"
+                        array_js_filenames.append("resources/js/" + protocol +'/'+ js_filename_alone[js_file_counter] + ".js")
+                    else:
+                        js_file_path = "resources/js/" + protocol + js_filename_alone[js_file_counter] + ".js"
+                        array_js_filenames.append("resources/js/" + protocol + js_filename_alone[js_file_counter] + ".js")
+                    
+                    # print("array_js_filenames:", array_js_filenames)
+                    # print("js_filename_alone[js_file_counter]", js_filename_alone[js_file_counter])
+                    ## testing new folder creation
+                    # if not os.path.exists('resources/js'):
+                    #     os.mkdir('resources/js')
+                    # if not os.path.exists('resources/js/' + protocol):
+                    #     os.mkdir('resources/js/' + protocol)
+                    output_file = Path(js_file_path)
+                    output_file.parent.mkdir(exist_ok=True, parents=True)
+
+                    
+                    encoding = chardet.detect(each_js)['encoding']
+                    # encoding = json.detect_encoding(each_js)
+
+                    # print("encoding:", encoding)
+                        # the_encoding = chardet.detect(rawdata)['encoding']
+
+                    # print(type(each_js))
+
+                    with open(js_file_path, "w", encoding=encoding, errors='ignore') as f:
+                    # with open(js_file_path, "w") as f:
+                        
+                        # text_file = open(js_file_path, "w+")
+                        f.write(each_js.decode(errors='ignore'))
+                        # f.close()
+                    
+                    js_file_counter += 1
+                except Exception as e:
+                    print("exception in getting js_file:", e)
+
+            if len(array_js_filenames) > 0:
+                # ip_doc['has_javascript'] = len(array_js_filenames)
+                print('has javascript')
+                # ip_doc['has_javascript'] = 1
+            else:
+                array_js_filenames = None
+            # to_append = {"type": protocol, "stderr": response.stderr, "stdout": response.stdout, "html_file_location":filepath, "js_file_location": array_js_filenames}
+            to_append["js_file_location"] = array_js_filenames
+            print("array_js_filenamess:", array_js_filenames)
+
+            # ip_doc['files_log'].append(to_append)
+        
+        except Exception as e:
+            # print(e)
+            print("===== grab_html_js function exception occured")
+            print("exception e:", e)
+            # if type(ip_doc['has_html']) == str:
+            #     ip_doc['has_html'] = 0
+            # if type(ip_doc['has_javascript']) == str:
+            #     ip_doc['has_javascript'] = 0
+            # # to_append = {"type": protocol, "stderr": response.stderr, "stdout": response.stdout, "html_file_location": None, "js_file_location" : None}
+            # to_append["js_file_location"] = None
+            # # to_append["exception"] = e
+            # ip_doc['files_log'].append(to_append)
+            # print(" ===== grab_html_js function exception end =====")
+            # # return ip_doc
+
+
+    # # ## else indicate its not good 
+    # else:
+    #     if type(ip_doc['has_html']) == str:
+    #         ip_doc['has_html'] = 0
+    #     if type(ip_doc['has_javascript']) == str:
+    #         ip_doc['has_javascript'] = 0
+    #     to_append = {"type": protocol, "stderr": response.stderr, "stdout": response.stdout, "html_file_location": None, "js_file_location" : None}
+    #     # ip_doc['files'] = [to_append]
+    #     ip_doc['files_log'].append(to_append)
+
+# print("CALL HTML JS IP_DOC:", ip_doc)
+print("===== grab_html_js function end =====")
